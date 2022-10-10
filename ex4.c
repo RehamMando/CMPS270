fce29068f49e2ee3409367c1ea0ed166f603224b
RehamMando <114496971+RehamMando@users.noreply.github.com>
2022-10-10 23:23:39 +0300

    Add files via upload

---
+++ ex4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+char ** merge(char* a1[], char* a2[], int size1, int size2){
+    int n = size1 + size2;
+    char **result;
+    result = (char**)malloc(sizeof(char*)*n);
+    for(int i = 0; i < n; i++){
+        
+    }
+}
